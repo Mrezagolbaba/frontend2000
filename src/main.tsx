cb1798862d5b6d8bb182759a69b8c6dbc8ea21b2
Mohamadrez@ <m.reza.golbaba@gmail.com>
2023-07-09 09:57:51 +0000

    remove unused file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css'
 import AppRouter from './AppRouter.tsx'
 import './assets/css/app.css';
 import './assets/css/custom.css';
