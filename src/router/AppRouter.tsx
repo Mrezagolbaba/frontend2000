61351983cfc7f9bd05805b9e826ecf4eafbf54c5
Mrezagolbaba <m.reza.golbaba@gmail.com>
2023-07-26 21:42:20 +0300

    add market to the route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -21,6 +21,7 @@ import BuySell from "../pages/buySell";
 import Information from "../pages/auth/information";
 import Forget from "../pages/auth/forget";
 import ProtectedRoute from "./ProtectedRoute";
+import Market from "../pages/market";
 // import AddToHomeBottomSheet from './components/AddToHomeBottomSheet';
 // import { isMobile } from 'react-device-detect';
 const AppRouter: React.FC = () => {
@@ -45,53 +46,56 @@ const AppRouter: React.FC = () => {
         />
         <Route
           path="/wallet"
-          element={<ProtectedRoute children={<Wallet/>} user={user} />}
+          element={<ProtectedRoute children={<Wallet />} user={user} />}
         />
         <Route
           path="/transactions/:id"
-          element={<ProtectedRoute children={<Transactions/>} user={user} />}
+          element={<ProtectedRoute children={<Transactions />} user={user} />}
         />
         <Route
           path="/setting"
-          element={<ProtectedRoute children={<Setting/>} user={user} />}
+          element={<ProtectedRoute children={<Setting />} user={user} />}
         />
         <Route
           path="/orders"
-          element={<ProtectedRoute children={<Orders/>} user={user} />}
+          element={<ProtectedRoute children={<Orders />} user={user} />}
         />
         <Route
           path="/notification"
-          element={<ProtectedRoute children={<Notifications/>} user={user} />}
+          element={<ProtectedRoute children={<Notifications />} user={user} />}
         />
         <Route
           path="/history"
-          element={<ProtectedRoute children={<History/>} user={user} />}
+          element={<ProtectedRoute children={<History />} user={user} />}
         />
         <Route
           path="/add-friends"
-          element={<ProtectedRoute children={<AddFriends/>} user={user} />}
+          element={<ProtectedRoute children={<AddFriends />} user={user} />}
         />
         <Route
           path="/transactions-invoice"
-          element={<ProtectedRoute children={<Invoices/>} user={user} />}
+          element={<ProtectedRoute children={<Invoices />} user={user} />}
         />
         <Route
           path="/profile"
-          element={<ProtectedRoute children={<Profile/>} user={user} />}
-        />  
-        <Route 
-          path="/support" 
-          element={<ProtectedRoute children={<Support/>} user={user} />}
+          element={<ProtectedRoute children={<Profile />} user={user} />}
+        />
+        <Route
+          path="/support"
+          element={<ProtectedRoute children={<Support />} user={user} />}
         />
         <Route
           path="/buy-sell"
-          element={<ProtectedRoute children={<BuySell/>} user={user} />}
+          element={<ProtectedRoute children={<BuySell />} user={user} />}
         />
         <Route
           path="/information"
-          element={<ProtectedRoute children={<Information/>} user={user} />}
+          element={<ProtectedRoute children={<Information />} user={user} />}
         />
-        
+        <Route
+          path="/market"
+          element={<ProtectedRoute children={<Market />} user={user} />}
+        />
 
         {/* Add a 404 route for pages that don't exist */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
