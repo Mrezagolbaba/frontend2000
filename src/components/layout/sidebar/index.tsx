412ab9e14b1e7b170631f7e3c04617288d3a1138
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-07-28 20:33:31 +0300

    remove unused code

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,6 +1,4 @@
 import LogoArsonex from "../../../assets/img/logo-arsonex.png";
-import Profile from "../../../assets/img/icons/profile.png";
-import Edit from "../../../assets/img/icons/edit.svg";
 import Home from "../../../assets/img/icons/home.svg";
 import Wallet from "../../../assets/img/icons/wallet.svg";
 import Order from "../../../assets/img/icons/paper.svg";
@@ -14,155 +12,13 @@ interface SidebarProps {
   isOpen: boolean;
   onSidebarToggle: () => void;
 }
-
-// const Sidebar: React.FC<SidebarProps> = ({ isOpen, onSidebarToggle }) => {
-//   const navigate = useNavigate();
-//   const activeItemRef = useRef(0);
-
-//   const handleItemClick = (index: number, path: string) => {
-//       activeItemRef.current = index;
-//       navigate(path);
-//       console.log(index,activeItemRef.current );
-//     }
-
-//   return (
-//     <aside className={`sidebar ${isOpen ? 'expanded' : ''}`} id="respMenu">
-// <button
-//   className="sidebar__close"
-//     onClick={()=>onSidebarToggle()}
-// >
-//   <span className="icon">
-//     <svg
-//       width="14"
-//       height="14"
-//       viewBox="0 0 14 14"
-//       fill="none"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       <path
-//         d="M13 1L1 13"
-//         stroke="#03041B"
-//         strokeWidth="1.5"
-//         strokeLinecap="round"
-//       />
-//       <path
-//         d="M13 13L1 0.999999"
-//         stroke="#03041B"
-//         strokeWidth="1.5"
-//         strokeLinecap="round"
-//       />
-//     </svg>
-//   </span>
-// </button>
-
-// <div className="sidebar-logo">
-//   <a >
-//     <img src={LogoArsonex} alt="" className="" />
-//   </a>
-// </div>
-
-// <div className="sidebar__user">
-//   <div className="sidebar__user__img">
-//     <img src={Profile} alt="" />
-//   </div>
-//   <div>
-//     <h6 className="sidebar__user__name">نام کاربری</h6>
-//     <a href="/profile" className="btn btn-transparent">
-//       <span className="icon">
-//         <img src={Edit} alt="" />
-//       </span>
-//       ویرایش
-//     </a>
-//   </div>
-// </div>
-
-//       <ul className="navbar">
-//         <li
-//           className={`navbar__item ${
-//             activeItemRef.current === 0 ? "navbar__item--active" : ""
-//           }`}
-//           onClick={() => handleItemClick(0, "/")}
-//         >
-//           <a >
-//             <span className="icon">
-//               <img src={Home} alt="" />
-//             </span>
-//             پیشخوان
-//           </a>
-//         </li>
-//         <li
-//           className={`navbar__item ${
-//             activeItemRef.current === 1 ? "navbar__item--active" : ""
-//           }`}
-//           onClick={() => handleItemClick(1, "/wallet")}
-//         >
-//           <a >
-//             <span className="icon">
-//               <img src={Wallet} alt="" />
-//             </span>
-//             کیف پول
-//           </a>
-//         </li>
-//         <li
-//           className={`navbar__item ${
-//             activeItemRef.current === 2 ? "navbar__item--active" : ""
-//           }`}
-//           onClick={() => handleItemClick(2, "/orders")}
-//         >
-//           <a >
-//             <span className="icon">
-//               <img src={Order} alt="" />
-//             </span>
-//             سفارشات من
-//           </a>
-//         </li>
-//         <li
-//           className={`navbar__item ${
-//             activeItemRef.current === 3 ? "navbar__item--active" : ""
-//           }`}
-//           onClick={() => handleItemClick(3, "/history")}
-//         >
-//           <a>
-//             <span className="icon">
-//               <img src={History} alt="" />
-//             </span>
-//             تاریخچه
-//           </a>
-//         </li>
-//         <li
-//           className={`navbar__item ${
-//             activeItemRef.current === 4 ? "navbar__item--active" : ""
-//           }`}
-//           onClick={() => handleItemClick(4, "/add-friends")}
-//         >
-//           <a href="/add-friends">
-//             <span className="icon">
-//               <img src={AddFriend} alt="" />
-//             </span>
-//             دعوت دوستان
-//           </a>
-//         </li>
-//         <li className="navbar__item navbar__item--logout">
-//           <a>
-//             <span className="icon">
-//               <img src={Logout} alt="" />
-//             </span>
-//             خروج از حساب
-//           </a>
-//         </li>
-//       </ul>
-//     </aside>
-//   );
-// };
-// export default Sidebar2;
 import React from "react";
 import { Menu } from "antd";
 
 import { Link } from "react-router-dom";
 
-
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onSidebarToggle }) => {
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState("");
 
   const location = useLocation();
 
@@ -176,58 +32,59 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onSidebarToggle }) => {
   };
   const items = [
     {
-      path: '/',
-      label: 'پیشخوان',
+      path: "/",
+      label: "پیشخوان",
       icon: <img src={Home} alt="" />,
     },
     {
-      path: '/wallet',
-      label: 'کیف پول',
+      path: "/wallet",
+      label: "کیف پول",
       icon: <img src={Wallet} alt="" />,
     },
     {
-      path: '/orders',
-      label: 'سفارشات من',
+      path: "/orders",
+      label: "سفارشات من",
       icon: <img src={Order} alt="" />,
     },
     {
-      path: '/history',
-      label: 'تاریخچه',
+      path: "/history",
+      label: "تاریخچه",
       icon: <img src={History} alt="" />,
     },
     {
-      path: '/add-friends',
-      label: 'دعوت دوستان',
+      path: "/add-friends",
+      label: "دعوت دوستان",
       icon: <img src={AddFriend} alt="" />,
     },
     {
-      path: '/logout',
-      label: 'خروج از حساب',
+      path: "/login",
+      label: "خروج از حساب",
       icon: <img src={Logout} alt="" />,
     },
   ];
   const renderItem = (item: any) => (
-    <Menu.Item
+    <div
       key={item.path}
-      className={`navbar__item ${activeItem === item.path ? 'navbar__item--active' : ''}`}
-      style={{ position: 'sticky', top: 0 }}
-      // icon={item.icon}
+      className={`navbar__item ${
+        activeItem === item.path ? "navbar__item--active" : ""
+      }`}
     >
       <Link to={item.path}>
         <span className="icon">{item.icon}</span>
         <span>{item.label}</span>
       </Link>
-    </Menu.Item>
+    </div>
   );
 
   return (
     <Menu
       onClick={handleClick}
       className={`sidebar ${isOpen ? "expanded" : ""}`}
-      style={{ width: 256 }}
+      // style={{ width: 256 }}
       defaultSelectedKeys={["0"]}
       defaultOpenKeys={["sub1"]}
       mode="inline"
+      style={{ width: 256, position: "sticky", top: 0 }}
       selectedKeys={[activeItem]}
     >
       <div className="sidebar__header">
@@ -262,23 +119,9 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onSidebarToggle }) => {
           </a>
         </div>
       </div>
-
-      <div className="sidebar__user">
-        <div className="sidebar__user__img">
-          <img src={Profile} alt="" />
-        </div>
-        <div>
-          <h6 className="sidebar__user__name">نام کاربری</h6>
-          <Link to="/profile" className="btn btn-transparent">
-            <span className="icon">
-              <img src={Edit} alt="" />
-            </span>
-            ویرایش
-          </Link>
-        </div>
-      </div>
-
+      <ul className="navbar">
       {items.map(renderItem)}
+      </ul>
     </Menu>
   );
 };
