410ba21dd1d2459e13bbd81459c156af28100c98
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-07-30 09:47:22 +0300

    remove unused

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/css/app.css';
 import './assets/css/custom.css';
-import './assets/vendor/owlcarousel/owl.carousel.min.css'
-import './assets/vendor/owlcarousel/owl.theme.default.min.css'
 import './assets/vendor/bootstrap-select/bootstrap-select.min.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import App from './App.tsx';
