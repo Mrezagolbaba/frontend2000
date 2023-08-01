63bf36e1bc0f629fa06304f7a5eb71a8a8b45115
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-01 14:30:38 +0300

    add error boundry

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import AppRouter from "./router/AppRouter";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ErrorBoundary from "./utils/error";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <ErrorBoundary>
+        <AppRouter />
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
