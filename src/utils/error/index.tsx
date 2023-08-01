553c19b002ee3e221d3985f09358a9c429e2b327
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-01 14:30:53 +0300

    error baoundry component

---
+++ src/utils/error/index.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+const ErrorBoundary: React.FC<ErrorBoundaryProps> = ({ children }) => {
+  useEffect(() => {
+    const errorHandler = (error: Error, errorInfo: React.ErrorInfo) => {
+      // Log the error or send it to your error monitoring service (e.g., Sentry).
+      console.error(error);
+
+      // You can also log the errorInfo to get more details about the error.
+      console.error('Error Info:', errorInfo);
+    };
+
+    window.addEventListener('error', errorHandler as any);
+
+    return () => {
+      window.removeEventListener('error', errorHandler as any);
+    };
+  }, []);
+
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+};
+
+export default ErrorBoundary;
