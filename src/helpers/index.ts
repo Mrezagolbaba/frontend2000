dfb1a4bebc1758b0c22ca157130451cec3a8f397
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-02 19:22:40 +0300

    create code country helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,25 +1,51 @@
-export function generateLabelValueArray(start:number, end:number) {
-    const resultArray = [];
-    for (let i = start; i <= end; i++) {
-      const label = i.toString();
-      resultArray.push({ label: label, value: label });
-    }
-    return resultArray;
+export function generateLabelValueArray(start: number, end: number) {
+  const resultArray = [];
+  for (let i = start; i <= end; i++) {
+    const label = i.toString();
+    resultArray.push({ label: label, value: label });
+  }
+  return resultArray;
 }
 export function generatePersianMonths() {
-    const persianMonths = [
-      { label: 'فروردین', value: '01' },
-      { label: 'اردیبهشت', value: '02' },
-      { label: 'خرداد', value: '03' },
-      { label: 'تیر', value: '04' },
-      { label: 'مرداد', value: '05' },
-      { label: 'شهریور', value: '06' },
-      { label: 'مهر', value: '07' },
-      { label: 'آبان', value: '08' },
-      { label: 'آذر', value: '09' },
-      { label: 'دی', value: '10' },
-      { label: 'بهمن', value: '11' },
-      { label: 'اسفند', value: '12' },
-    ];
-    return persianMonths;
+  const persianMonths = [
+    { label: 'فروردین', value: '01' },
+    { label: 'اردیبهشت', value: '02' },
+    { label: 'خرداد', value: '03' },
+    { label: 'تیر', value: '04' },
+    { label: 'مرداد', value: '05' },
+    { label: 'شهریور', value: '06' },
+    { label: 'مهر', value: '07' },
+    { label: 'آبان', value: '08' },
+    { label: 'آذر', value: '09' },
+    { label: 'دی', value: '10' },
+    { label: 'بهمن', value: '11' },
+    { label: 'اسفند', value: '12' },
+  ];
+  return persianMonths;
+}
+
+export const countries = [
+  // {
+  //   label: '🇺🇸 +1',
+  //   value: '+1',
+  // },
+  {
+    // code: 'United Kingdom',
+    label: `🇬🇧        +44`,
+    value: '+44',
+  },
+  {
+    value: '+61',
+    label: `🇦🇺      +61`,
+  },
+  {
+
+    value: '+1',
+    label: '🇨🇦 +1',
+  },
+  {
+    value: '+98',
+    label: '🇮🇷 +98',
   }
+  // Add more countries as needed
+];
