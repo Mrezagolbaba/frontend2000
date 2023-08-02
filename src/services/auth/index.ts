1e18f860fe4f54e2afd711433f8752883aaaf4c2
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-02 19:23:57 +0300

    create index auth

---
+++ src/services/auth/index.ts
@@ -0,0 +1,9 @@
+import login from './signin';
+import useCreateUser from './signup';
+import signup from './signup';
+import useLogin from './signin';
+
+export {
+    login
+    , useCreateUser, signup, useLogin
+};
