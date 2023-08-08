dadb339fda157a61be1b6bf878fbd660113532f5
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-08 06:55:09 +0300

    update otp mobile functions

--- src/pages/auth/otp/OtpMobile.tsx
+++ src/pages/auth/otp/OtpMobile.tsx
@@ -1,6 +1,52 @@
 import HeadAuth from "../../../components/layout/headAuth";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { PhoneNumberMask } from "../../../helpers";
+import OtpInput from "../../../components/OTP";
+import { useState } from "react";
+import { send } from "vite";
+import { resendOtp, sendOtp } from "../../../services/auth";
 
 const OtpMobile: React.FC = () => {
+  const router = useNavigate();
+  const location = useLocation();
+  let phoneNumber = location.state.phoneNumber;
+  const [otpCode, setOtpCode] = useState("");
+
+  const handleOtpChange = (code: string) => {
+    setOtpCode(code);
+  };
+
+  const handleSendOtp = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    const data: OtpData = {
+      code: otpCode,
+      type: "AUTH",
+      method: "PHONE",
+    };
+    const response = await sendOtp(data);
+    if (response.status === 200) {
+      router("/");
+    }
+  };
+
+  const handleResend = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    const data: OtpData = {
+      code: otpCode,
+      type: "AUTH",
+      method: "PHONE",
+    };
+    const response = await resendOtp(data);
+    if (response.status === 200) {
+      router("/");
+    }
+  };
+
   return (
     <div className="auth-wrapper" id="root">
       <main className="auth-main">
@@ -12,26 +58,34 @@ const OtpMobile: React.FC = () => {
               <div className="auth-summary">
                 <p className="auth-text text-end">
                   کد تایید ارسال شده به
-                  <span className="d-ltr d-inline-block">0912****789</span>
+                  <span className="d-ltr d-inline-block">
+                    {PhoneNumberMask({
+                      phoneNumber,
+                    })}
+                  </span>
                   را وارد کنید.
                 </p>
-                <button className="btn-simple auth-resend">ارسال مجدد کد</button>
+                <button
+                  onClick={handleResend}
+                  className="btn-simple auth-resend"
+                >
+                  ارسال مجدد کد
+                </button>
               </div>
 
               <form action="" className="auth-form">
                 <div className="mb-4">
-                  <div className="code-input-control">
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                    <input type="number" className="form-control d-ltr control-auto-focus" placeholder="-" />
-                  </div>
+                  <OtpInput onChange={handleOtpChange} />
                 </div>
                 <div className="auth-footer">
                   <div className="mb-3">
-                    <button type="submit" className="btn btn-primary auth-submit">ثبت نام</button>
+                    <button
+                      onClick={handleSendOtp}
+                      type="submit"
+                      className="btn btn-primary auth-submit"
+                    >
+                      ثبت نام
+                    </button>
                   </div>
                   <div className="auth-edit-mobile text-center">
                     <a href="/register">ویرایش شماره همراه</a>
@@ -44,5 +98,5 @@ const OtpMobile: React.FC = () => {
       </main>
     </div>
   );
-}
+};
 export default OtpMobile;
