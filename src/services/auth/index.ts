661946d35006e910fde0eb397066c0fabfa5af81
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-08 06:55:26 +0300

    add request otp

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,8 +1,20 @@
-import login from './signin';
-import useCreateUser from './signup';
-import signup from './signup';
-import useLogin from './signin';
-import useForgetPassword from './forgot';
-import forgetPassword  from './forgot';
+import login from "./signin";
+import useCreateUser from "./signup";
+import signup from "./signup";
+import useLogin from "./signin";
+import useForgetPassword from "./forgot";
+import forgetPassword from "./forgot";
+import { resendOtp, sendOtp, useResendOtp, useSendOtp } from "./otp";
 
-export {login, useCreateUser, signup, useLogin, useForgetPassword, forgetPassword};
+export {
+  login,
+  useCreateUser,
+  signup,
+  useLogin,
+  useForgetPassword,
+  forgetPassword,
+  sendOtp,
+  useSendOtp,
+  resendOtp,
+  useResendOtp,
+};
---
+++ src/services/auth/otp.ts
@@ -0,0 +1,39 @@
+import {
+  UseMutationOptions,
+  UseMutationResult,
+  useMutation,
+} from "react-query";
+import request from "../adapter";
+import toast from "react-hot-toast";
+
+export const sendOtp = async (data: any) => {
+  try {
+    const response = await request.post("auth/verify-2fa", data);
+    return response.data;
+  } catch (error: any) {
+    toast.error(error.response.data.message);
+  }
+};
+
+// Hook for send OTP mutation
+export const useSendOtp = (
+  options?: UseMutationOptions<any, Error, any, any>,
+): UseMutationResult<any, Error, any, any> => {
+  return useMutation(sendOtp, options);
+};
+
+export const resendOtp = async (data: any) => {
+  try {
+    const response = await request.post("auth/verify-2fa", data);
+    return response.data;
+  } catch (error: any) {
+    toast.error(error.response.data.message);
+  }
+};
+
+// Hook for resend OTP mutation
+export const useResendOtp = (
+  options?: UseMutationOptions<any, Error, any, any>,
+): UseMutationResult<any, Error, any, any> => {
+  return useMutation(resendOtp, options);
+};
