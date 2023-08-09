91d225c921c767885fe5fc90133c52020bd5fe1c
Mohamadreza Golbaba <mreza@dev.tribes.work>
2023-08-09 08:07:42 +0300

    update sentry config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,7 +6,10 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
+    org: "arsonex",
+    project: "frontend"
+  })],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
