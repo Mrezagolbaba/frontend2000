2b2e8cfe84bf21d03171156c5a35a125ce955e26
mrezagolbaba <m.reza.golbaba@gmail.com>
2023-08-17 08:27:10 +0330

    set validation list for password input

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -1,10 +1,17 @@
 import FloatInput from "components/Input/FloatInput";
 import React, { useState } from "react";
 import { FieldErrors, RefCallBack } from "react-hook-form";
-import { BsEye, BsEyeSlash } from "react-icons/bs";
+import {
+  BsCheckCircle,
+  BsExclamationCircle,
+  BsEye,
+  BsEyeSlash,
+} from "react-icons/bs";
 import { CiLock } from "react-icons/ci";
 
 import "./style.sass";
+import { List } from "antd";
+import { isPasswordValid, passwordListValidation } from "helpers";
 
 interface Props {
   onChange: (...event: any[]) => void;
@@ -13,36 +20,70 @@ interface Props {
   name: string;
   ref: RefCallBack;
   errors?: FieldErrors;
+  hasShowHint?: boolean;
 }
 
-const PasswordInput = ({ name, value, ref, onChange, errors }: Props) => {
+const PasswordInput = ({
+  name,
+  value,
+  ref,
+  onChange,
+  errors,
+  hasShowHint = false,
+}: Props) => {
+  const [passValid, setPassValid] = useState<boolean[] | []>([]);
   const [hasShowPass, setHasShowPass] = useState<boolean>(false);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (hasShowHint) setPassValid(() => isPasswordValid(e.target.value));
+    onChange?.(e);
+  };
+
   return (
-    <FloatInput
-      type={hasShowPass ? "text" : "password"}
-      name={name}
-      value={value}
-      label="رمز عبور"
-      onChange={onChange}
-      inputProps={{
-        ref: ref,
-        size: "large",
-        prefix: <CiLock />,
-        suffix: !hasShowPass ? (
-          <BsEye
-            onClick={() => setHasShowPass(true)}
-            className="show-password"
-          />
-        ) : (
-          <BsEyeSlash
-            onClick={() => setHasShowPass(false)}
-            className="show-password"
-          />
-        ),
-        status: errors?.[name]?.message ? "error" : undefined,
-      }}
-    />
+    <>
+      <FloatInput
+        type={hasShowPass ? "text" : "password"}
+        name={name}
+        value={value}
+        label="رمز عبور"
+        onChange={handleChange}
+        inputProps={{
+          ref: ref,
+          size: "large",
+          prefix: <CiLock />,
+          suffix: !hasShowPass ? (
+            <BsEye
+              onClick={() => setHasShowPass(true)}
+              className="show-password"
+            />
+          ) : (
+            <BsEyeSlash
+              onClick={() => setHasShowPass(false)}
+              className="show-password"
+            />
+          ),
+          status: errors?.[name]?.message ? "error" : undefined,
+        }}
+      />
+      {hasShowHint && (
+        <List
+          className="password-validation-check"
+          itemLayout="horizontal"
+          dataSource={passwordListValidation}
+          renderItem={(item, index: number) => (
+            <List.Item key={index}>
+              <List.Item.Meta
+                className={passValid[index] ? "check" : "unCheck"}
+                avatar={
+                  passValid[index] ? <BsCheckCircle /> : <BsExclamationCircle />
+                }
+                title={item.title}
+              />
+            </List.Item>
+          )}
+        />
+      )}
+    </>
   );
 };
 
