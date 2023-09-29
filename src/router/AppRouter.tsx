4f9b5e5a77f48a422c8bbc21348436b7692604b4
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2023-09-29 13:34:58 +0330

    fix routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,4 @@
-import React, { lazy } from "react"; //  { useState }
-
-import { Refine } from "@refinedev/core";
-import dataProvider from "@refinedev/simple-rest";
-import routerProvider from "@refinedev/react-router-v6";
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useRoutes,
-} from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
+import { useRoutes } from "react-router-dom";
 import LoginPage from "pages/auth/signin";
 import LoginEmailPage from "pages/auth/signin/EmailSignin";
 import SignupPage from "pages/auth/signup";
@@ -21,27 +9,14 @@ import ForgetPasswordWithEmail from "pages/auth/reset-password/WithEmail";
 import ResetPassword from "pages/auth/reset-password/ResetPassword";
 
 import HomePage from "pages/Home";
-import Dashboard from "pages/dashboard";
-import { WalletList } from "pages/dashboard/wallet";
-import { TransactionList } from "pages/transactions";
-import Setting from "pages/setting";
-import Orders from "pages/orders";
-import Notifications from "pages/notifications";
-import History from "pages/history";
-import AddFriends from "pages/add-friends";
-import Invoices from "pages/invoices";
-import Profile from "pages/profile";
-import Support from "pages/support";
-import BuySell from "pages/buySell";
+
 import Information from "pages/auth/information";
-import Market from "pages/market";
-import request from "services/adapter";
+
 import CoinPage from "pages/coins";
 import DashboardRouter from "./DashboardRouter";
 import ComingSoon from "pages/ComingSoon";
 
 export default function AppRouter() {
-
   return useRoutes([
     {
       path: "/",
