3630cdb12bbe68e204e825737b06a6a5f70bc816
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2023-09-29 05:23:33 +0330

    create deposit and withdraw with Rial in wallet section

---
+++ src/pages/wallet/Rial/Deposit/ShebaForm.tsx
@@ -0,0 +1,170 @@
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+import {
+  Button,
+  Col,
+  FormGroup,
+  FormText,
+  Input,
+  InputGroup,
+  Label,
+  Row,
+} from "reactstrap";
+import CopyInput from "components/Input/CopyInput";
+import saman from "assets/img/bank/Saman.svg";
+
+import wallet from "pages/wallet/style.module.scss";
+import { useState } from "react";
+import { AlertInfo } from "components/AlertWidget";
+
+type ShebaFormType = {
+  accountName: string;
+  ShebaNumber: string;
+  depositId: string;
+  bankName: string;
+};
+const ShebaForm = () => {
+  const [hasLevel2, setHasLevel2] = useState<boolean>(true);
+
+  const resolver = yupResolver(
+    Yup.object().shape({
+      accountName: Yup.string().required(),
+      ShebaNumber: Yup.string().required(),
+      depositId: Yup.string().required(),
+      bankName: Yup.string().required(),
+    })
+  );
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    formState: { errors },
+  } = useForm<ShebaFormType>({
+    mode: "onChange",
+    defaultValues: {
+      accountName: "سامانه تبادل سریع آرسونیکس",
+      ShebaNumber: "IR100100100100100100100101000",
+      depositId: "9124312329593000",
+      bankName: "بانک سامان",
+    },
+    resolver,
+  });
+  const onSubmit = async (data: ShebaFormType) => {
+    console.log(data);
+  };
+
+  return hasLevel2 ? (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Row>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="accountName"
+            control={control}
+            render={({ field: { name, value, onChange, ref } }) => (
+              <FormGroup>
+                <Label htmlFor={name}>نام صاحب حساب:</Label>
+                <Input
+                  disabled
+                  type="text"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  ref={ref}
+                />
+              </FormGroup>
+            )}
+          />
+        </Col>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="ShebaNumber"
+            control={control}
+            render={({ field: { name, value } }) => (
+              <FormGroup>
+                <Label htmlFor={name}> شماره شبا:</Label>
+                <CopyInput text={value} />
+                <FormText>حداقل مبلغ واریز: 500 هزارتومان</FormText>
+              </FormGroup>
+            )}
+          />
+        </Col>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="depositId"
+            control={control}
+            render={({ field: { name, value } }) => (
+              <FormGroup>
+                <Label htmlFor={name}> شناسه واریز:</Label>
+                <CopyInput text={value} />
+                <FormText>کارمزد واریز بین بانکی: صفر تومان</FormText>
+              </FormGroup>
+            )}
+          />
+        </Col>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="bankName"
+            control={control}
+            render={({ field: { name, value, onChange, ref } }) => (
+              <FormGroup>
+                <Label htmlFor={name}>نام صاحب حساب:</Label>
+                <InputGroup className={wallet["bank-name-input"]}>
+                  <div className={wallet["input-prepend"]}>
+                    <span className={wallet["bank-logo"]}>
+                      <img
+                        src={saman}
+                        className="img-responsive bank-logo"
+                        alt="TL"
+                      />
+                    </span>
+                  </div>
+                  <Input
+                    className={wallet.input}
+                    disabled
+                    type="text"
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                </InputGroup>
+              </FormGroup>
+            )}
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={12} className="mt-3">
+          <div className="col-lg-12 mt-3">
+            <span className={wallet["iban-text"]}>
+              1- از حساب&zwnj;هایی که در پروفایل خود وارد کرده&zwnj;اید امکان
+              واریز وجود دارد.{" "}
+            </span>
+            <span className={wallet["iban-text"]}>
+              2- شناسه واریز را در قسمت توضیحات یا شناسه واریز وارد نمایید.{" "}
+            </span>
+            <span className={wallet["iban-text"]}>
+              3- تمامی روش&zwnj;های پرداخت بجز روش پل مورد تایید می&zwnj;باشد.{" "}
+            </span>
+          </div>
+        </Col>
+      </Row>
+    </form>
+  ) : (
+    <Row>
+      <AlertInfo
+        text="واریز بین بانکی در صورتی برای شما فعال می‌شود که به سطح دو کاربری ارتقا پیدا کنید."
+        hasIcon={true}
+      />
+      <div className="text-center mt-3">
+        <Button color="primary" type="button" onClick={() => {}} outline>
+          ارتقاع سطح کاربری
+        </Button>
+      </div>
+    </Row>
+  );
+};
+
+export default ShebaForm;
---
+++ src/pages/wallet/Rial/Withdraw/index.tsx
@@ -0,0 +1,166 @@
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { AlertInfo } from "components/AlertWidget";
+import {
+  Button,
+  Col,
+  FormFeedback,
+  FormGroup,
+  FormText,
+  Label,
+  Row,
+} from "reactstrap";
+import DropdownInput, { OptionType } from "components/Input/Dropdown";
+
+import pasargad from "assets/img/bank/Pasargad.svg";
+import saman from "assets/img/bank/Saman.svg";
+
+import wallet from "pages/wallet/style.module.scss";
+import Currency from "components/Input/CurrencyInput";
+
+type WithdrawType = {
+  sheba: string;
+  amount: string;
+};
+
+export default function Withdraw() {
+  const optionList: OptionType[] = [
+    {
+      content: (
+        <div className={wallet["items-credit"]}>
+          <span className={wallet["items-credit__icon"]}>
+            <img alt="بانک پاسارگارد" src={pasargad} className="bank-svg" />
+          </span>
+          <span>5859831058254847</span>
+        </div>
+      ),
+      value: "5859831058254847",
+    },
+    {
+      content: (
+        <div className={wallet["items-credit"]}>
+          <span className={wallet["items-credit__icon"]}>
+            <img alt="بانک سامان" src={saman} className="bank-svg" />
+          </span>
+
+          <span className="text">502229103949549395</span>
+        </div>
+      ),
+      value: "502229103949549395",
+    },
+  ];
+
+  const resolver = yupResolver(
+    Yup.object().shape({
+      sheba: Yup.string().required(),
+      amount: Yup.string().required(),
+    })
+  );
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    formState: { errors },
+  } = useForm<WithdrawType>({
+    mode: "onChange",
+    defaultValues: {
+      sheba: "502229103949549395",
+      amount: "",
+    },
+    resolver,
+  });
+  const onSubmit = async (data: WithdrawType) => {
+    console.log(data);
+  };
+  return (
+    <form className="px-3" onSubmit={handleSubmit(onSubmit)}>
+      <Row>
+        <AlertInfo
+          hasIcon
+          text="برای افزایش میزان برداشت، احراز هویت سطح دو را تکمیل نمایید."
+        />
+        <AlertInfo
+          hasIcon
+          text="تسویه حساب با بانک‌های سامان، صادرات، کشاورزی، پارسیان، سپه، شهر، ملی، اقتصادنوین، آینده، پاسارگارد، ملت و تجارت سریع‌تر انجام می‌شود."
+        />
+        <AlertInfo
+          hasIcon
+          text="تمامی درخواست‌ها بعد از ثبت برداشت وارد چرخه پایا شده و در اولین سیکل یا سیکل بعدی روز‌های کاری برای شما واریز می‌شود."
+        />
+      </Row>
+      <Row>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="sheba"
+            control={control}
+            render={({ field: { name, value } }) => (
+              <FormGroup className="position-relative">
+                <div className="d-flex flex-row justify-content-between">
+                  <Label htmlFor={name}> واریز به شبا: </Label>
+                  <a href="#">
+                    <span className="full-withraw mt-1">افزودن حساب جدید</span>
+                  </a>
+                </div>
+                <DropdownInput
+                  id={name}
+                  value={value}
+                  onChange={(val) => setValue(name, val)}
+                  options={optionList}
+                  hasError={Boolean(errors?.[name])}
+                />
+                {errors?.[name] && (
+                  <FormFeedback tooltip>
+                    {errors[name]?.message}sdfsdfsfd
+                  </FormFeedback>
+                )}
+                <FormText>
+                  سقف باقی مانده برداشت روزانه این حساب: 100,000,000 تومان
+                </FormText>
+              </FormGroup>
+            )}
+          />
+        </Col>
+        <Col xs={12} lg={6}>
+          <Controller
+            name="amount"
+            control={control}
+            render={({ field: { name, value } }) => (
+              <FormGroup className="position-relative">
+                <div className="d-flex flex-row justify-content-between">
+                  <Label htmlFor={name}>مبلغ برداشت: </Label>
+                  <a href="#">
+                    <span className="full-withraw mt-1">
+                      حداکثر قابل برداشت
+                    </span>
+                  </a>
+                </div>
+                <Currency
+                  name={name}
+                  value={value}
+                  placeholder="مبلغ را به تومان وارد کنید"
+                  hasError={Boolean(errors?.[name])}
+                />
+                {errors?.[name] && (
+                  <FormFeedback tooltip>{errors[name]?.message}</FormFeedback>
+                )}
+                <span className="d-flex flex-row justify-content-between">
+                  <FormText>موجودی شما: 12,000,000 تومان</FormText>
+                  <FormText>کارمزد برداشت بانکی: 2,000 هزارتومان</FormText>
+                </span>
+              </FormGroup>
+            )}
+          />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <div className="text-center">
+          <Button color="primary" outline type="submit">
+            ثبت درخواست برداشت
+          </Button>
+        </div>
+      </Row>
+    </form>
+  );
+}
