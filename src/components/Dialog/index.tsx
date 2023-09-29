3016ce3036ac79af1589e3b19aad67644bec7dee
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2023-09-29 05:19:26 +0330

    create dialog component

---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import dialog from "./style.module.scss"
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  hasCloseButton?: boolean;
+  children: React.ReactNode;
+};
+
+function Dialog({
+  isOpen,
+  onClose,
+  title,
+  hasCloseButton = false,
+  children,
+}: Props) {
+  return (
+    <Modal className={dialog["modal"]} isOpen={isOpen} toggle={onClose} size="lg" centered backdrop fade>
+      <ModalHeader className={dialog["modal-header"]} toggle={hasCloseButton ? onClose : undefined}>
+        {title}
+      </ModalHeader>
+      <ModalBody>{children}</ModalBody>
+    </Modal>
+  );
+}
+
+export default Dialog;
