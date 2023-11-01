c3b10cb3ddd15026cb2c01345e909223b2a1be02
Mohamadrez@ <m.reza.golbaba@gmail.com>
2023-11-02 00:17:46 +0300

    create api

--- src/services/currencySwap/index.ts
+++ src/services/currencySwap/index.ts
@@ -2,17 +2,45 @@ import request from "services/adapter";
 import { toast } from "react-hot-toast";
 
 type RequestType = {
-    userId: string;
-  };
-export const getCurrencySwap = async ({userId}:RequestType) => {
-    console.log(userId)
-    try {
-        const response = await request.get(`/currency-swaps`);
-        return response.data;
-    } catch (error: any) {
-        const errorMessage =
-        error.response?.data?.message || "An error occurred. Please try again.";
-      toast.error(errorMessage, { position: "bottom-left" });
-      throw new Error(errorMessage);
-    }
+  userId: string;
+};
+export const getCurrencySwap = async ({ userId }: RequestType) => {
+  console.log(userId)
+  try {
+    const response = await request.get(`/currency-swaps`);
+    return response.data;
+  } catch (error: any) {
+    const errorMessage =
+      error.response?.data?.message || "An error occurred. Please try again.";
+    toast.error(errorMessage, { position: "bottom-left" });
+    throw new Error(errorMessage);
+  }
+}
+interface RequestExchange {
+  sourceCurrencyCode: string,
+  sourceAmount: string,
+  destinationCurrencyCode: string,
+  feeCurrencyCode: string
+
+}
+export const exchangeCurrencySwap = async ({
+  sourceCurrencyCode,
+  sourceAmount,
+  destinationCurrencyCode,
+  feeCurrencyCode
+}: RequestExchange) => {
+  try {
+    const response = await request.post(`/currency-swaps`, {
+      sourceCurrencyCode,
+      sourceAmount,
+      destinationCurrencyCode,
+      feeCurrencyCode
+    })
+    return response.data;
+  } catch (error: any) {
+    const errorMessage =
+      error.response?.data?.message || "An error occurred. Please try again.";
+    toast.error(errorMessage, { position: "bottom-left" });
+    throw new Error(errorMessage);
+  }
 }
