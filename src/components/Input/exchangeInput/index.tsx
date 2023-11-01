10f36090e545099e6d9752cba73d2c1bbdecc250
Mohamadrez@ <m.reza.golbaba@gmail.com>
2023-11-02 00:16:47 +0300

    update exchange input

--- src/components/Input/exchangeInput/index.tsx
+++ src/components/Input/exchangeInput/index.tsx
@@ -12,7 +12,7 @@ import { Cu } from "react-flags-select";
 
 const options = [
     {
-        value: "ریال",
+        value: "IRR",
         label: (
             <>
                 <img
@@ -32,48 +32,9 @@ const options = [
             </>
         ),
     },
+   
     {
-        value: "بیت کوین",
-        label: (
-            <>
-                <img
-                    src={bitcoin}
-                    alt=""
-                    className="bs-icon"
-                    width={20}
-                    height={20}
-                    style={{ marginLeft: "5px" }}
-                />
-                <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
-                    بیت کوین
-                </span>
-
-            </>
-        ),
-    },
-    {
-        value: "اتریوم",
-        label: (
-            <>
-                <img
-                    src={ethereum}
-                    alt=""
-                    className="bs-icon"
-                    width={20}
-                    height={20}
-                    style={{ marginLeft: "5px" }}
-                />
-                <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
-                    {" "}
-                    اتریوم
-                </span>
-
-
-            </>
-        ),
-    },
-    {
-        value: "لیر",
+        value: "TRY",
         label: (
             <>
                 <img width={20} height={20} src={Lira} alt="" className="bs-icon" style={{ marginLeft: "5px" }} />
@@ -86,28 +47,68 @@ const options = [
             </>
         ),
     },
-    {
-        value: "یورو",
-        label: (
-            <>
-                <img
-                    src={Euro}
-                    alt=""
-                    className="bs-icon"
-                    width={20} height={20}
-                    style={{ marginLeft: "5px" }}
-                />
-                <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
-                    {" "}
-                    یورو
-                </span>
+    // {
+    //     value: "BTC",
+    //     label: (
+    //         <>
+    //             <img
+    //                 src={bitcoin}
+    //                 alt=""
+    //                 className="bs-icon"
+    //                 width={20}
+    //                 height={20}
+    //                 style={{ marginLeft: "5px" }}
+    //             />
+    //             <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
+    //                 بیت کوین
+    //             </span>
+
+    //         </>
+    //     ),
+    // },
+    // {
+    //     value: "ETH",
+    //     label: (
+    //         <>
+    //             <img
+    //                 src={ethereum}
+    //                 alt=""
+    //                 className="bs-icon"
+    //                 width={20}
+    //                 height={20}
+    //                 style={{ marginLeft: "5px" }}
+    //             />
+    //             <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
+    //                 {" "}
+    //                 اتریوم
+    //             </span>
 
 
-            </>
-        ),
-    },
+    //         </>
+    //     ),
+    // },
+    // {
+    //     value: "EUR",
+    //     label: (
+    //         <>
+    //             <img
+    //                 src={Euro}
+    //                 alt=""
+    //                 className="bs-icon"
+    //                 width={20} height={20}
+    //                 style={{ marginLeft: "5px" }}
+    //             />
+    //             <span style={{ fontFamily: "IRANSansWeb", fontSize: "12px" }}>
+    //                 {" "}
+    //                 یورو
+    //             </span>
+
+
+    //         </>
+    //     ),
+    // },
     {
-        value: "تتر",
+        value: "USDT",
         label: (
             <>
                 <img
@@ -135,6 +136,7 @@ type Props = {
     placeholder?: string;
     decimalsLimit?: number;
     hasError?: boolean;
+    onChangeCoin?: (value) => void;
 };
 
 export default function ExchangeInput({
@@ -144,12 +146,13 @@ export default function ExchangeInput({
     onChange,
     decimalsLimit = 0,
     placeholder,
+    onChangeCoin,
 }: Props) {
     const [isOpen, setIsOpen] = useState(false);
     const [selected, setSelected] = useState(options[0]);
 
-    const handleSelect = (selectedValue: string) => {
-        onChange && onChange(selectedValue);
+    const handleSelectCoin = (selectedValue: string) => {
+        onChangeCoin && onChangeCoin(selectedValue);
         setSelected(options.find((option) => option.value === selectedValue)!);
         setIsOpen(false);
     };
@@ -159,7 +162,7 @@ export default function ExchangeInput({
                 className={s.Input}
                 type="text"
                 value={value}
-                onChange={(e) => onChange && onChange(e.target.value)}
+                onValueChange={(value) => value && onChange?.(value)}
             />
             <div className="dropdown">
                 <button
@@ -176,7 +179,7 @@ export default function ExchangeInput({
                         <li key={index}>
                             <button className={s.button} onClick={(e) => {
                                 e.preventDefault();
-                                handleSelect(option.value)
+                                handleSelectCoin(option.value)
                             }
                             }>{option.label}</button>
                         </li>
