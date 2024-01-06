dfea5fd2d36a9adaadfec5ce973c64ff6bd3d3f0
mehdi.a <39931701+mahdiabaszadeh@users.noreply.github.com>
2024-01-07 00:01:01 +0330

    Create vite.config.ts
    Signed-off-by: mehdi.a <39931701+mahdiabaszadeh@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import sassDts from "vite-plugin-sass-dts";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "process.env.REACT_APP_BASE_URL": JSON.stringify(
+      "https://dev-api.arsonex.market/v1"
+    ),
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: "arsonex",
+      project: "frontend",
+      telemetry: false,
+      authToken:""
+    }),
+    sassDts(), // Add the SASS plugin to the plugins array
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+});
