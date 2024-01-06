3002ae7488493edcfaaba904d812d9db01bb7b8f
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-01-06 23:48:56 +0330

    try for fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import AppRouter from "./router/AppRouter";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./utils/error";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 function App() {
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "GTM-WRSW3TKG", // Replace with your GTM container ID
+    };
+
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ErrorBoundary>
