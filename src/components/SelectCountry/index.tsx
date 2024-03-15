1bfb4b71508938a69da90a76bac8649c1c2ceda8
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-03-15 07:28:37 -0400

    add two flag

--- src/components/SelectCountry/index.tsx
+++ src/components/SelectCountry/index.tsx
@@ -8,6 +8,8 @@ import australia from "assets/img/flags/Australia.svg";
 import canada from "assets/img/flags/Canada.svg";
 import iran from "assets/img/flags/Iran.svg";
 import turkey from "assets/img/flags/Turkey.svg";
+import estonia from "assets/img/flags/estonia.png";
+import germany from "assets/img/flags/germany.png";
 
 type Props = {
   onChange: (...event: any[]) => void;
@@ -19,29 +21,38 @@ type Props = {
 const SelectCountry = ({ name, value, onChange, errors }: Props) => {
   const options = [
     {
-      value: "90",
-      label: (
-        <div className="flag-container">
-          <span>+90</span>
-          <img src={turkey} alt="Turkey-flag" width="25" />
-        </div>
-      ),
-    },
-    {
       value: "98",
       label: (
         <div className="flag-container">
           <span>+98</span>
-          <img src={iran} alt="Iran-flag" width="25" />
+          <img src={iran} alt="Iran-flag" />
         </div>
       ),
     },
     {
-      value: "44",
+      value: "90",
       label: (
         <div className="flag-container">
-          <span>+44</span>
-          <img src={england} alt="England-flag" width="25" />
+          <span>+90</span>
+          <img src={turkey} alt="Turkey-flag" />
+        </div>
+      ),
+    },
+    {
+      value: "372",
+      label: (
+        <div className="flag-container">
+          <span>+372</span>
+          <img src={estonia} alt="Estonia-flag" />
+        </div>
+      ),
+    },
+    {
+      value: "49",
+      label: (
+        <div className="flag-container">
+          <span>+49</span>
+          <img src={germany} alt="Germany-flag" />
         </div>
       ),
     },
@@ -50,7 +61,16 @@ const SelectCountry = ({ name, value, onChange, errors }: Props) => {
       label: (
         <div className="flag-container">
           <span>+1</span>
-          <img src={canada} alt="Canada-flag" width="25" />
+          <img src={canada} alt="Canada-flag" />
+        </div>
+      ),
+    },
+    {
+      value: "44",
+      label: (
+        <div className="flag-container">
+          <span>+44</span>
+          <img src={england} alt="England-flag" />
         </div>
       ),
     },
@@ -59,7 +79,7 @@ const SelectCountry = ({ name, value, onChange, errors }: Props) => {
       label: (
         <div className="flag-container">
           <span>+61</span>
-          <img src={australia} alt="Australia-flag" width="25" />
+          <img src={australia} alt="Australia-flag" />
         </div>
       ),
     },
