c6b5913fd94dfc0ac941c7354f295ce7f64842b8
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-03-19 08:26:41 -0400

    fix ticket

--- src/pages/dashboard/support/index.tsx
+++ src/pages/dashboard/support/index.tsx
@@ -1,5 +1,5 @@
-import { Card, CardBody, CardHeader, CardTitle } from "reactstrap";
-import TicketModal from "./addModal";
+import { Button, Card, CardBody, CardHeader, CardTitle } from "reactstrap";
+import TicketModal from "./AddModal";
 import { useState } from "react";
 import { useGetTicketsQuery } from "store/api/ticket";
 import { ITicket } from "types/ticket";
@@ -8,66 +8,75 @@ import { Link } from "react-router-dom";
 
 const Support = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { data, error, isLoading } = useGetTicketsQuery();
-  // console.log(data)
+  const { data, isLoading } = useGetTicketsQuery();
   return (
     <section className="page page-support">
       <Card className="custom-card currencies-online-rates card-secondary">
         <CardHeader className="d-flex flex-row justify-content-between align-items-center">
           <CardTitle tag="h5">پشتیبانی</CardTitle>
           <div className="card-action">
-            <button
-              className="btn btn-outline-primary"
+            <Button
+              className="mx-2 px-4"
+              color="primary"
+              size="sm"
+              style={{
+                fontSize: ".87rem",
+              }}
+              onClick={() => setIsModalOpen(true)}
+            >
+              مرکز راهنمایی
+            </Button>
+            <Button
+              color="primary"
+              outline
+              size="sm"
+              style={{
+                fontSize: ".87rem",
+              }}
+              className="px-4"
               onClick={() => setIsModalOpen(true)}
             >
               ارسال تیکت
-            </button>
+            </Button>
           </div>
         </CardHeader>
         <CardBody>
-          <div className="table-responsive">
-            <table className="table table-borderless ">
+          <div
+            className="table-responsive"
+            style={{
+              fontSize: ".87rem",
+            }}
+          >
+            <table
+              className={`table table-borderless ${data?.length === 0 ? "table-modern" : "table-striped"}`}
+            >
               <thead>
                 <tr>
-                  <th scope="col" className="text-center">
+                  <th
+                    scope="col"
+                    style={{ color: "#03041b66" }}
+                    className="text-center"
+                  >
                     تاریخ ارسال
                   </th>
-                  <th scope="col" className="text-center">
+                  <th
+                    scope="col"
+                    style={{ color: "#03041b66" }}
+                    className="text-center"
+                  >
                     عنوان تیکت
                   </th>
-                  <th scope="col" className="text-center">
+                  <th
+                    scope="col"
+                    style={{ color: "#03041b66" }}
+                    className="text-center"
+                  >
                     وضعیت
                   </th>
                   <th scope="col" className="text-center"></th>
                 </tr>
               </thead>
-              <tbody>
-                {data?.map((item: ITicket) => (
-                  <tr>
-                    <td className="text-center">
-                      <span className="d-ltr d-block">
-                        {moment(item?.createdAt)
-                          .locale("fa")
-                          .format("DD MMMM YYYY")}
-                      </span>
-                    </td>
-                    <td className="text-center">{item.subject}</td>
-                    <td className="text-center">
-                      {item.status === "OPEN" ? (
-                        <span className="text-success"> در دست بررسی </span>
-                      ) : (
-                        <span className="text-danger">خاتمه یافته</span>
-                      )}
-                    </td>
-                    <td className="text-center table-new__actions">
-                      <Link to={`/dashboard/support/details/${item.id}`}>
-                        مشاهده جزئیات
-                      </Link>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-              {isLoading && (
+              {isLoading ? (
                 <tbody>
                   <tr>
                     <td className="placeholder-glow">
@@ -82,6 +91,17 @@ const Support = () => {
                     <td className="placeholder-glow">
                       <div className="placeholder col-12 rounded" />
                     </td>
+                  </tr>
+                  <tr>
+                    <td className="placeholder-glow">
+                      <div className="placeholder col-12 rounded" />
+                    </td>
+                    <td className="placeholder-glow">
+                      <div className="placeholder col-12 rounded" />
+                    </td>
+                    <td className="placeholder-glow">
+                      <div className="placeholder col-12 rounded" />
+                    </td>
                     <td className="placeholder-glow">
                       <div className="placeholder col-12 rounded" />
                     </td>
@@ -99,27 +119,32 @@ const Support = () => {
                     <td className="placeholder-glow">
                       <div className="placeholder col-12 rounded" />
                     </td>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
                   </tr>
-                  <tr>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
-                    <td className="placeholder-glow">
-                      <div className="placeholder col-12 rounded" />
-                    </td>
-                  </tr>
+                </tbody>
+              ) : (
+                <tbody>
+                  {data?.map((item: ITicket, index: number) => (
+                    <tr key={index}>
+                      <td className="text-center">
+                        <span className="d-ltr d-block">
+                          {moment(item?.createdAt)
+                            .locale("fa")
+                            .format("DD MMMM YYYY")}
+                        </span>
+                      </td>
+                      <td className="text-center">{item.subject}</td>
+                      <td className="text-center">
+                        {item.status === "OPEN" ? (
+                          <span className="text-success"> باز</span>
+                        ) : (
+                          <span className="text-danger">بسته </span>
+                        )}
+                      </td>
+                      <td className="text-center table-new__actions">
+                        <Link to={`details/${item.id}`}>مشاهده جزئیات</Link>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               )}
             </table>
