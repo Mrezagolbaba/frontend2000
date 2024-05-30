cd1dbf5e5d4ab6e55146ef29eb0de26c6bd3e9fe
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-05-30 07:52:15 +0300

    update sentry config

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { useEffect } from "react";
+import * as Sentry from "@sentry/react";
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration.tsx";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import * as Sentry from "@sentry/react";
 import { persister, store } from "store/store.ts";
-import TagManager from 'react-gtm-module';
+import TagManager from "react-gtm-module";
 
 import "assets/scss/index.scss";
 
@@ -14,31 +20,42 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { PersistGate } from "redux-persist/integration/react";
 
-Sentry.init({
-  dsn: "https://471c3edaafc4ceda011aec9a5263fc88@o208701.ingest.sentry.io/4505642858119168",
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: [
-        "localhost",
-        /^https:\/\/dev-api\.arsonex\.market\//,
-        /^https:\/\/staging-api\.arsonex\.market\//,
-        /^https:\/\/api\.arsonex\.com\//,
-      ],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  autoSessionTracking: true,
-});
 
 const tagManagerArgs = {
-  gtmId: 'GTM-WRSW3TKG', // Replace with your GTM container ID
+  gtmId: "GTM-WRSW3TKG", // Replace with your GTM container ID
 };
+Sentry.init({
+  dsn: "https://2874ea1120cfbc93c7e66586b9afb209@o4506688238780416.ingest.us.sentry.io/4507340175966208",
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+    Sentry.browserTracingIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    // /^https:\/\/dev-api\.paydirham\.me\//,
+    // /^https:\/\/staging-api\.paydirham\.me\//,
+    /^https:\/\/api\.arsonex\.com\//,
+  ],
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 TagManager.initialize(tagManagerArgs);
 
@@ -49,6 +66,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 serviceWorkerRegistration.register();
