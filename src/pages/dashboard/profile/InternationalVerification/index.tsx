a2d9adf2deb60492677b8495e3ea4b9b993d2c73
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-06-09 16:55:50 +0330

    update kyc-international steps

--- src/pages/dashboard/profile/InternationalVerification/index.tsx
+++ src/pages/dashboard/profile/InternationalVerification/index.tsx
@@ -2,13 +2,23 @@ import FinalStep from "./FinalStep";
 import NameStep from "./NameStep";
 import ResidentCardStep from "./ResidentCardStep";
 import { Button, Col, Container, Row } from "reactstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useGetVerificationsQuery } from "store/api/profile-management";
 
 import profile from "assets/scss/dashboard/profile.module.scss";
 
+export enum REJECTION_REASON {
+  INVALID_RESIDENCE_PERMIT = "INVALID_RESIDENCE_PERMIT",
+  EXPIRED_RESIDENCE_PERMIT = "EXPIRED_RESIDENCE_PERMIT",
+  POOR_QUALITY_RESIDENCE_PERMIT_FRONT = "POOR_QUALITY_RESIDENCE_PERMIT_FRONT",
+  POOR_QUALITY_RESIDENCE_PERMIT_BACK = "POOR_QUALITY_RESIDENCE_PERMIT_BACK",
+}
 export default function InternationalVerification() {
   // ==============|| States ||================= //
   const [step, setStep] = useState<0 | 1 | 2>(0);
+
+  // ==============|| Hooks ||================= //
+  const { data: userVerifications, isSuccess } = useGetVerificationsQuery();
 
   // ==============|| Handlers ||================= //
   const renderSteps = () => {
@@ -22,6 +32,18 @@ export default function InternationalVerification() {
         return <NameStep successHandler={() => setStep(1)} />;
     }
   };
+
+  // ==============|| Life Cycle||================= //
+  useEffect(() => {
+    if (isSuccess && userVerifications) {
+      const international = userVerifications.find(
+        (item) => item.type === "KYC_INTERNATIONAL_SERVICES",
+      );
+      if (international?.status === "REJECTED") {
+        setStep(1);
+      }
+    }
+  }, [isSuccess, userVerifications]);
 
   // ==============|| Render ||================= //
   return (
