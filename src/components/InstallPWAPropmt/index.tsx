9b7fde887dd49413cb8b13fdc6bae4d4335fd4de
JalalHamed <iamjalalhamed@gmail.com>
2024-07-09 16:27:01 +0330

    separate styling concerns

--- src/components/InstallPWAPropmt/index.tsx
+++ src/components/InstallPWAPropmt/index.tsx
@@ -4,6 +4,7 @@ import { isMobile, isTablet, isFirefox, isIOS } from "react-device-detect";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import Logo from "assets/img/logo-arsonex.png";
 import IOSShare from "assets/icons/IOSShare";
+import "./styles.scss";
 
 const InstallPWAPrompt: FC = () => {
   const [modal, setModal] = useState<boolean>(false);
@@ -21,67 +22,36 @@ const InstallPWAPrompt: FC = () => {
 
   return (
     <Modal isOpen={modal} toggle={toggle} centered>
-      <div style={{ backgroundColor: "#eee", borderRadius: "12px" }}>
-        <ModalHeader
-          toggle={toggle}
-          style={{
-            padding: "25px 0 10px",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
+      <div className="wrapper">
+        <ModalHeader toggle={toggle} className="pb-2">
           <div className="d-inline-flex flex-row align-items-center gap-3">
             <img src={Logo} loading="lazy" height="35px" width="35px" />
-            <p className="m-0" style={{ fontSize: "16px" }}>
-              نصب آرسونیکس
-            </p>
+            <p className="m-0 f16">نصب آرسونیکس</p>
           </div>
         </ModalHeader>
         <ModalBody>
-          <p style={{ fontSize: "12px", textAlign: "justify" }}>
+          <p className="description">
             برنامه را روی دستگاه خود نصب کنید تا در هر زمان به راحتی به آن
             دسترسی داشته باشید.
           </p>
           {isIOS && (
-            <ol
-              style={{
-                fontSize: "12px",
-                marginTop: "-10px",
-                padding: 0,
-                paddingRight: "12px",
-              }}
-            >
+            <ol className="ordered-list">
               <li className="mb-1 mt-4">
                 روی{" "}
-                <span
-                  style={{
-                    backgroundColor: "#fff",
-                    padding: "4px 0",
-                    borderRadius: "4px",
-                  }}
-                >
+                <span className="icon">
                   <IOSShare style={{ width: "25px", height: "30px" }} />
                 </span>{" "}
                 کلیک کنید.
               </li>
               <li>
-                <span
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#fff",
-                    padding: "2px",
-                    borderRadius: "4px",
-                  }}
-                >
-                  Add to Home Screen
-                </span>{" "}
-                را انتخاب کنید.
+                <span className="icon p-1">Add to Home Screen</span> را انتخاب
+                کنید.
               </li>
             </ol>
           )}
         </ModalBody>
         {!isIOS && (
-          <ModalFooter style={{ justifyContent: "flex-start" }}>
+          <ModalFooter className="justify-content-start">
             <Button color="primary" outline onClick={promptToInstall}>
               نصب
             </Button>
