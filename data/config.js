7a2e84ba19c49218f090545b2b579b754f480a08
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-08-31 02:22:08 +0330

    base url

--- data/config.js
+++ data/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://dev-api.paydirham.me/v1/';
+export const BASE_URL = 'https://api-dev-new.paydirham.me/v1/';
 export const ADMIN_ADDRESS = 'https://dev-dash.paydirham.me';
